88e21a1e5f1a02aad84213c8ddc3989e57ab281c
Gunnar Babicz <gunnarbabicz@yahoo.com>
2023-01-02 00:29:07 -0700

    Trying a popup about me window

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('About').addEventListener("click", function() {
+	document.querySelector('.bg-modal').style.display = "flex";
+});
+
+document.querySelector('.close').addEventListener("click", function() {
+	document.querySelector('.bg-modal').style.display = "none";
+});
